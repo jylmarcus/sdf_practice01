54f7186e7cf2c85f41dcd40db5941da9ba80ff82
jylmarcus <maplecow@gmail.com>
2023-05-08 12:57:18 +0800

    Testing code

---
+++ src/main/java/sg/edu/nus/iss/Main.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.iss;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        String test = System.getProperty("port");
+        System.out.println(test);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
